bba0b2ed0893cad0606d7b4d00efd3d094356d99
Sebastian Gonzalez <seba.gnh@gmail.com>
2022-09-21 22:36:44 -0300

    actualizando path / para realizar pruebas

--- src/App.js
+++ src/App.js
@@ -9,26 +9,89 @@ import Tienda from './components/Tienda';
 function App() {
   return (
     <Fragment>
-      <div className="container">
-        <nav className="navbar navbar-dark bg-primary justify-content-center">
-          <NavLink to={"/Inicio"}  className="navbar-brand btn btn-primary btn-lg" href="#">Inicio</NavLink>
+      <DivContenedor className="container">
+        <Nav className="navbar navbar-dark bg-primary justify-content-center">
+          <NavLink to={"/"} className="navbar-brand btn btn-primary btn-lg" href="#">Inicio</NavLink>
           <NavLink to={"/blog"}  className="navbar-brand btn btn-primary btn-lg" href="#">Blog</NavLink>
           <NavLink to={"/tienda"}  className="navbar-brand btn btn-primary btn-lg" href="#">Tienda</NavLink>        
-        </nav>
+        </Nav>
 
         <div className="">
           <Routes>
             <Route path="/" element={<Inicio />} />
-            <Route path="/inicio" element={<Inicio />} />
             <Route path="/blog" element={<Blog />} />
             <Route path="/tienda" element={<Tienda />} />
             <Route path='*' element={<Inicio />} />
           </Routes>
         </div>
-      </div>
+      </DivContenedor>
     </Fragment>
   );
 }
 
 export default App;
 
+
+
+const DivContenedor = styled.div`
+  //background: #404040;
+  //margin: 40px 0;
+  //border-radius: 10px;
+  //box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+
+const Nav = styled.nav`
+  //width: 100%;
+  //text-align: center;
+  //background: #092c4c;
+  //grid-column: span 2;
+  border-radius: 3px;
+//
+  //a {
+  //    color: #fff;
+  //    display: inline-block;
+  //    padding: 15px 20px;
+  //}
+//
+  //a:hover {
+  //    background: #1d85e8;
+  //    text-decoration: none;
+  //}
+`;
+
+
+
+
+
+
+const Contenedor2 = styled.div`
+  max-width: 1000px;
+  padding: 40px;
+  width: 90%;
+  display: grid;
+  gap: 20px;
+  grid-template-columns: 2fr 1fr;
+  background: #fff;
+  margin: 40px 0;
+  border-radius: 10px;
+  box-shadow: 0px 0px 5px rgba(129, 129, 129, 0.1);
+`;
+
+const Menu = styled.nav`
+  width: 100%;
+  text-align: center;
+  background: #092c4c;
+  grid-column: span 2;
+  border-radius: 3px;
+
+  a {
+      color: #fff;
+      display: inline-block;
+      padding: 15px 20px;
+  }
+
+  a:hover {
+      background: #1d85e8;
+      text-decoration: none;
+  }
+`;
